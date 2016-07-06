4ab32b32ea8cadd0911b527f156d8f5a1945f935
sabsolvei <sabrinaveiga@hotmail.com>
2016-07-06 19:43:43 -0300

    Instruccion If - Ejercicios 1 y 2 Resueltos

--- 2-InstruccionIf/jsInstruccionIF (1).js
+++ 2-InstruccionIf/jsInstruccionIF (1).js
@@ -2,11 +2,16 @@ function Mostrar()
 {
 //tomo la edad  
 var edad= document.getElementById('edad').value;
-edad= parseInt(edad);
+edad= parseInt(edad);//averiguar como hacer para que acepte solo nùmeros el campo
 
 if (edad==15)
 	{
 		alert("Niña bonita");
 	}
+
+else
+{
+	alert("Mujer bonita");
+}
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (2).js
+++ 2-InstruccionIf/jsInstruccionIF (2).js
@@ -1,7 +1,15 @@
 function Mostrar()
 {
 //tomo la edad  
+var edad = document.getElementById('edad').value;
+edad=parseInt(edad)
+if (edad>=18)
+{
+	alert("La persona es mayor de edad");
+}
 
-
-
+else
+{
+	alert("La persona es menor de edad");
+}
 }//FIN DE LA FUNCIÓN
