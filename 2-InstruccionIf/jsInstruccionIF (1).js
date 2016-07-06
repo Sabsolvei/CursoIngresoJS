d54ea7e96876f6228691a16b3ba5959f74c8cb7e
sabsolvei <sabrinaveiga@hotmail.com>
2016-07-06 18:55:28 -0300

    Instruccion if Ejercicio 1 Resuelto

--- 2-InstruccionIf/jsInstruccionIF (1).js
+++ 2-InstruccionIf/jsInstruccionIF (1).js
@@ -1,7 +1,12 @@
 function Mostrar()
 {
 //tomo la edad  
+var edad= document.getElementById('edad').value;
+edad= parseInt(edad);
 
-
+if (edad==15)
+	{
+		alert("Niña bonita");
+	}
 
 }//FIN DE LA FUNCIÓN
