e04b8f5e5f16d58e936288803637456373b7fe9a
sabsolvei <sabrinaveiga86@gmail.com>
2016-07-10 23:22:03 -0300

    Instruccion if - Ejercicios 9 y 10 resueltos

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,6 +1,24 @@
 function Mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
-	
+var nota;
+nota = Math.floor((Math.random()*10)+1);
+
+if (nota <11 && nota>8)
+{
+	alert("Su nota es: "+nota+". EXCELENTE");
+}
+	else
+	{
+		if (nota >3 && nota <9)
+		{
+			alert("Su nota es: "+nota+". Aprobado");
+		}
+			else
+			{
+				alert("Su nota es: "+nota+". Vamos, la próxima se puede");
+			}
+	}
+
 
 }//FIN DE LA FUNCIÓN
