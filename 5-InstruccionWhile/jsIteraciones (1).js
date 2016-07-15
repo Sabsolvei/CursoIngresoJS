634e7ee683a00969feaa7020b1a62dd8828df3f3
sabsolvei <sabrinaveiga@hotmail.com>
2016-07-15 20:25:18 -0300

    While-Ej 1 y 2

--- 5-InstruccionWhile/jsIteraciones (1).js
+++ 5-InstruccionWhile/jsIteraciones (1).js
@@ -1,6 +1,92 @@
 function Mostrar()
 {
-	alert('iteración while');
+	//alert('iteración while');
+
+var contador;
+contador = 0;
+
+/*NO HACERLO ASÌ
+if(contador<10)
+{
+	alert("mensaje " + contador);
+contador = contador + 1;
+}
+if(contador<10)
+{
+	alert("mensaje " + contador);
+contador = contador + 1;
+}*/
+
+
+//WHILE
+/*while(contador<3)
+{
+	alert("mensaje " + contador);
+
+contador = contador + 1;
+}*/
+
+
+/*//WHILE TRUE
+while(true)
+{
+	
+	contador = contador + 1;//se pone en esa ubicación cuando quiere inicializar en 1
+
+	alert("mensaje " + contador);
+
+	if(contador==3)
+	{
+		break;
+	}
+}//fin while*/
+
+/*
+while(true)
+{
+	alert("mensaje " + contador);
+
+	if(contador==3)
+	{
+		break;
+	}
+contador = contador + 1; //se pone en esa ubicación cuando quiere inicializar en 0
+
+}//fin while*/
+
+/*while(1)
+{
+	contador=contador+1
+	if(contador==3)
+	{
+		continue;//cuando el contador llega a 3 vuelve a arrancar(con el contador como está)
+	}
+	alert("while "+contador)
+	if(contador==6)
+	{
+		break;//cuando llega al 6 sale del While
+	}
+	alert("Fin "+contador);
+}*/
+/*while(1)
+{	
+	if(contador==3)
+	{
+		continue;//cuando el contador llega a 3 vuelve a arrancar(con el contador como está)
+	}
+	alert("while "+contador)
+	if(contador==6)
+	{
+		break;//cuando llega al 6 sale del While
+	}
+	alert("Fin "+contador);
+	contador=contador+1//con contador aca,cuando llegue al 3 vuelve arriba y nunca llega abajo para sumar.
+}*/
+
+
+
+
+
 
 
 }//FIN DE LA FUNCIÓN
