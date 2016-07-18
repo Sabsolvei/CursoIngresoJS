e023abb64a425bcc79c471a6733fe4d8abf60c9d
sabsolvei <sabrinaveiga@hotmail.com>
2016-07-18 16:35:24 -0300

    TPs Ej 5

--- 8-TPs/jsAdivinaElNumero1.js
+++ 8-TPs/jsAdivinaElNumero1.js
@@ -7,20 +7,44 @@ secreto del 1 al 100, en la pantalla del juego
 “Usted es un ganador!!! y en solo X intentos”.
 de no ser igual se debe informar si “falta…”  para llegar al número secreto  o si “se pasó…”  del número secreto.
 */
+
 var numeroSecreto; 
-var contadorIntentos;
+var contadorIntentos=0;
+var numeroIngresado;
 
 function comenzar()
 {
-	//Genero el número RANDOM entre 1 y 100
-	 
-		//alert(numeroSecreto );
 	
+	numeroSecreto = Math.floor((Math.random()*20)+1);
 
 }
+	function verificar()
+	{
+		numeroIngresado=document.getElementById('numero').value;
+		numeroIngresado=parseInt(numeroIngresado);
+		contadorIntentos=parseInt(contadorIntentos)+1;
+		alert(numeroIngresado);
+		alert(numeroSecreto);
+		if(numeroSecreto<numeroIngresado)
+		{
+			alert('Se pasó por '+(numeroIngresado-numeroSecreto)+' decimales para acertar');
+		}
+		else if(numeroSecreto>numeroIngresado)
+		{
+			alert('Le faltaron '+(numeroSecreto-numeroIngresado)+' decimales para acertar');
+		}
+		else if(numeroSecreto==numeroIngresado)
+		{
+			alert('¡Usted es un ganador. Y en solo '+contadorIntentos +' intentos!');
+		}
 
-function verificar()
-{
-	
-	
-}
+		document.getElementById('numero').value=numeroIngresado;
+		document.getElementById('intentos').value=contadorIntentos;
+
+	}
+
+
+
+
+
+
